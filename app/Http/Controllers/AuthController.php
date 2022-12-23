e1517765f522312785ddb5d1bef5b20baf2ab5b5
LizaCastDiaz <lizacastdiaz@gmail.com>
2022-12-23 20:46:09 +0100

    AuthController add to the proyect

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,12 @@
+<?php
+namespace App\Http\Controllers;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Validator;
+use Tymon\JWTAuth\Facades\JWTAuth;
+
+class AuthController extends Controller
+{
+
+}
